818ee3f88ff60c0b2100c7799871e1cff193e4b5
pnirtep <pnirtep@gmail.com>
2020-01-01 16:11:54 +0300

    2

---
+++ BlogDBmodels/create_functions.py
@@ -0,0 +1,28 @@
+from BlogDBmodels.models import *
+
+
+def create_user(name, email):
+    Base.metadata.create_all(engine)
+    user = User(nickname=name, email=email)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(user)
+    session.commit()
+
+
+def create_post(title, text, user_email):
+    Base.metadata.create_all(engine)
+    post = Post(title=title, text=text, user_email=user_email)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(post)
+    session.commit()
+
+
+def create_tag(tag_name):
+    Base.metadata.create_all(engine)
+    tag = Tag(tag_name=tag_name)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(tag)
+    session.commit()
